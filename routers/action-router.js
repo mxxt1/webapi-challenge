795e91086642cdcc9416b874794785bf1c0e218d
mxxt1 <matt@dustcloudlabs.com>
2019-11-01 13:01:15 -0400

    STILL FLYING BLIND! Action and Project routes created. Pushing work and switching to laptop

--- routers/action-router.js
+++ routers/action-router.js
@@ -5,20 +5,76 @@ const db = require('../data/helpers/actionModel.js')
 
 
 //get
-// router.get('/actions', (req, res) => {
-//     console.log(`get from action router`);
-// });
+router.get('/', (req, res) => {
+    console.log(`get from action router`);
+    db.get()
+    .then(actions => {
+        console.log(actions)
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(500).json({Error: err})
+    })
+});
+
+router.get('/:id', (req, res) => {
+    console.log(`action get with id`)
+    db.get(req.params.id)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
 
 
 //post
+
+router.post('/', (req, res) => {
+    console.log(`action post`)
+    db.insert(req.body)
+    .then(newAction => {
+        res.status(200).json(newAction)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
 
 
 
 //put
 
+router.put('/:id', (req, res) => {
+    console.log(`inside action put`)
+    db.update(req.params.id, req.body)
+    .then(updated => {
+        if(updated === null){
+            res.status(401).json(`Id does not exist`)
+        } else{
+            res.status(400).json(updated)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+
 
 
 //delete
+
+router.delete(':/id', (req, res) => {
+    console.log(`inside action delete`)
+    db.remove(req.params.id)
+    .then(count => {
+        res.status(200).json(`deleted ${count} actions`)
+    })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const server = express()
 
 // reference routes
 const projectRouter = require('./routers/project-router.js')
-// const actionRouter = require('./routers/action-router.js')
+const actionRouter = require('./routers/action-router.js')
 
 
 // use middleware
 server.use(express.json())
 server.use(helmet())
 
-
-
-
+//  use routes
+server.use('/projects', projectRouter)
+server.use('/actions', actionRouter)
 
 
 //root route
@@ -25,10 +25,6 @@ server.get('/', (req, res) => {
     // res.status(200).json({message: "Server active!"})
     res.send({message: "Server active!"});
 })
-
-//  use routes
-server.use('/projects', projectRouter)
-// server.use('/actions', actionRouter)
 
 
 //export
